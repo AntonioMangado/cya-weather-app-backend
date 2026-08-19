e459ebb53144244ddd1d9ee4bc0fcdd626a646c9
AntonioMangado <antonio.mangado@gmail.com>
2026-08-19 22:28:09 +0200

    clean up

--- src/weather/weather.constants.ts
+++ src/weather/weather.constants.ts
@@ -1,7 +1,5 @@
 export const WEATHER_API_URL = 'https://api.weatherapi.com/v1/forecast.json';
 
-// Current day + 3 days of forecast, per the plan.
 export const FORECAST_DAYS = 4;
 
-// WeatherAPI error code for "No location found matching parameter q".
 export const WEATHER_API_ERROR_CODE_LOCATION_NOT_FOUND = 1006;
